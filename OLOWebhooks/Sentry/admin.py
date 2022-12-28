e726b7ff1a7274e7a488d180c9524ab1995e5f5c
Tim Scott <118086790+timdev-dash@users.noreply.github.com>
2022-12-28 11:33:40 -0500

    Beginning of web service setup
    Using simple test app for now, working towards setup of webhook handling.

--- OLOWebhooks/Sentry/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from Sentry.models import WebHook, Location
-
-# Register your models here.
-
-admin.site.register(WebHook)
-admin.site.register(Location)
--- OLOWebhooks/Sentry/urls.py
+++
@@ -1,4 +0,0 @@
-# List of URLS used by the Sentry Django app
-
-urlpatterns = [
-]
