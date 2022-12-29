ae9efe842fc5df71359d3bad23249657419df467
Tim Scott <118086790+timdev-dash@users.noreply.github.com>
2022-12-29 10:37:30 -0500

    Update __init__.py
    Added lines for testing webhook site location

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from dbmanager import Database
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secretkey123"
@@ -7,13 +8,39 @@ app.config['SECRET_KEY'] = "secretkey123"
 def home():
         return jsonify({'Message': "Testing1"})
 
-@app.route("/catering") # creating reception point for catering webhooks
+@app.route("/catering", methods=['POST', 'GET']) # creating reception point for catering webhooks
 def catering_receipt():
-        pass
+        ## Setting default database settings for insert functions
+        db_name = 'catering_in.sqlite'
+        table_name = 'Incoming'
+        db = Database(db_name)
 
-@app.route("/monitor") # creating receotpion point for mealtime webhooks
+        ## Setting logic to receive and file received webhooks 
+        if request.method == 'POST':
+                text_to_insert = str(request.json)
+                db.db_insert(table_name, [text_to_insert,])
+                
+                ### Sending the required HTTP 200 response communicating receipt of webhook
+                return '', 200
+        elif request.method == 'GET':
+                return 'Test Succesful', 201
+
+
+
+@app.route("/monitor", methods=['POST']) # creating receotpion point for mealtime webhooks
 def monitor_receipt():
-        pass
+        ## Setting default database settings for insert functions
+        db_name = 'online_in.sqlite'
+        table_name = 'Incoming'
+        db = Database(db_name)
+
+        ## Setting logic to receive and file received webhooks 
+        if request.method == 'POST':
+                text_to_insert = str(request.json)
+                db.db_insert(table_name, [text_to_insert,])
+                
+                ### Sending the required HTTP 200 response communicating receipt of webhook
+                return '', 200
 
 if __name__ == "__main__":
     app.run()
